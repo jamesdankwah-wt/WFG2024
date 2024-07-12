0773651d0415b4d7607cdbf3f29cc6aba682ea9f
James Dankwah <james.dankwah@willowtreeapps.com>
2024-07-11 23:43:10 -0400

    changed

--- c4k-app/next.config.mjs
+++ c4k-app/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://szzhocxbgebegpyvhzko.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6emhvY3hiZ2ViZWdweXZoemtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3MTkwMDgsImV4cCI6MjAzNjI5NTAwOH0.lzXbdNDyW4JHvRVpOvbTexfrwG3WrgluJcU-izhWTB8',
+    },
+    images: {
+        
+    
+    },
+};
 
 export default nextConfig;
